86309c1e32c4f2a33ec7ebe9c5ea964c6a2f14e5
Miura <inacio.miuraj@gmail.com>
2025-02-23 20:57:18 -0300

    Added error handling and mutating data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,8 @@ const FormSchema = z.object({
   date: z.string(),
 });
  
+// Create Invoice
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
@@ -25,16 +27,21 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
- 
-    await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
 
+    try {
+      await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+    } catch (error) {
+      // We'll log the error to the console for now
+      console.error(error);
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
 
+// Update Invoice
 
   // Use Zod to update the expected types
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
@@ -48,12 +55,27 @@ export async function createInvoice(formData: FormData) {
    
     const amountInCents = amount * 100;
    
-    await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-   
+    try {
+      await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+    } catch (error) {
+      // We'll log the error to the console for now
+      console.error(error);
+    }
+  }
+
+// Delete Invoice 
+
+  export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice');
+    try {
+      await sql`DELETE FROM invoices WHERE id = ${id}`;
+    } catch (error) {
+      // We'll log the error to the console for now
+      console.error(error);
+    }
     revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
   }
