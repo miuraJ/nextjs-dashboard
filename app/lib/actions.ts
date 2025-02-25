8e648ac2161c01acda7521b77fc19bec88040ab2
Miura <inacio.miuraj@gmail.com>
2025-02-24 23:59:25 -0300

    Updated server validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -83,13 +83,23 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
   // Use Zod to update the expected types
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
   
-  export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+  export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    // const { customerId, amount, status } = UpdateInvoice.parse({
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
    
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    
     try {
@@ -101,7 +111,11 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
     } catch (error) {
       // We'll log the error to the console for now
       console.error(error);
+      return { message: 'Database Error: Failed to Update Invoice.' };
     }
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
   }
 
 // Delete Invoice 
