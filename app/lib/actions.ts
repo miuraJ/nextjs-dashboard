5e445b35edfb139dfa25dde4763464a3ac54a3dc
Miura <inacio.miuraj@gmail.com>
2025-02-25 21:36:53 -0300

    Added authentication and one user

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -130,3 +133,24 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
     }
     revalidatePath('/dashboard/invoices');
   }
+
+// Authentication
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
